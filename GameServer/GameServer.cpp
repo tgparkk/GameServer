d792edfc248d57e666ed48abb4c84730d7cc9385
tgparkk <sttgpark@gmail.com>
2024-11-02 12:59:12 +0900

    create std::thread

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -5,7 +5,47 @@
 #include <iostream>
 #include "CorePch.h"
 
+#include <thread>
+
+void HelloThread()
+{
+    std::cout << "Hello Thread" << std::endl;
+}
+
+void HelloThread_2(int32 num)
+{
+    std::cout << num << std::endl;
+}
+
 int main()
 {
-    HelloWorld();
+    // System Call (OS 커널 요청)
+    //std::cout << "Hello World" << std::endl;
+
+    std::thread t;
+    t = std::thread(HelloThread);
+
+    int32 count = t.hardware_concurrency(); // CPU 코어 개수?
+    auto id = t.get_id(); // 쓰레드마다 id
+    t.detach(); // std::thread 객체에서 실제 쓰레드를 분리
+
+    if (t.joinable())
+        t.join();
+
+
+    std::vector<std::thread> v;
+    for (int32 i = 0; i < 10; i++)
+    {
+        v.push_back(std::thread(HelloThread_2, i));
+    }
+
+    for (int32 i = 0; i < 10; i++)
+    {
+        if (v[i].joinable())
+            v[i].join();
+    }
+
+
+    std::cout << "Hello Main" << std::endl;
+
 }
