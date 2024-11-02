9a4b4e7a77ac81b2a7576b8d1c87c398b2ac8b3f
tgparkk <sttgpark@gmail.com>
2024-11-02 14:52:06 +0900

    std::lock

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -6,35 +6,65 @@
 #include "CorePch.h"
 #include <thread>
 #include <atomic>
+#include <mutex>
 
-std::atomic<int32> sum = 0;
+std::vector<int32> v;
 
-void Add()
+// Mutual Exclusive (상호배타적)
+std::mutex m;
+
+// RAII (Resource Acquisition Is Intialization)
+template<typename T>
+class LockGuard
 {
-	for (int32 i = 0; i < 1'000'000; i++)
+public:
+	LockGuard(T& m)
 	{
-		sum.fetch_add(1); // sum++;  같음 , 오버로딩
+		_mutex = &m;
+		_mutex->lock();
 	}
-}
-
-void Sub()
-{
-	for (int32 i = 0; i < 1'000'000; i++)
+	~LockGuard()
 	{
-		sum.fetch_add(-1); // sum--;  같음 , 오버로딩
+		_mutex->unlock();
+	}
+
+private:
+	T* _mutex;
+};
+
+void Push()
+{
+	std::lock_guard<std::mutex> lockguard(m);
+	for (int32 i= 0; i < 10000; i++)
+	{
+		// 자물쇠 잠그기
+		//m.lock();
+		//LockGuard<std::mutex> lockguard(m);
+		//std::lock_guard<std::mutex> lockguard(m);
+
+		//std::unique_lock<std::mutex> uniqueLock(m, std::defer_lock);
+
+
+		// lock 을 뒤로 미를수있음
+		//uniqueLock.lock();
+
+		v.push_back(i);
+
+		//if (i == 5000)
+		//	break;
+		
+		// 자물쇠 풀기
+		//m.unlock();
 	}
 }
 
 int main()
 {
-	Add();
-	Sub();
-	std::cout << sum << std::endl;
+	std::thread t1(Push);
+	std::thread t2(Push);
 
-	std::thread t1(Add);
-	std::thread t2(Sub);
 	t1.join();
 	t2.join();
 
-	std::cout << sum << std::endl;
+	std::cout << v.size() << std::endl;
 }
