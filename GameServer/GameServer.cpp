6b3389c3044a0a446de91372f05f836befa044a5
tgparkk <sttgpark@gmail.com>
2024-11-10 12:00:23 +0900

    reader_writer lock

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -7,24 +7,72 @@
 #include <future>
 #include "ThreadManager.h"
 
-CoreGlobal Core;
+class TestLock
+{
+	USE_LOCK;
 
-void ThreadMain()
+public:
+	int32 TestRead()
+	{
+		READ_LOCK;
+
+		if (_queue.empty())
+			return -1;
+
+		return _queue.front();
+	}
+
+	void TestPush()
+	{
+		WRITE_LOCK;
+
+		_queue.push(rand() % 100);
+	}
+
+	void TestPop()
+	{
+		WRITE_LOCK;
+
+		if (_queue.empty() == false)
+			_queue.pop();
+	}
+
+private:
+	queue<int32> _queue;
+};
+
+TestLock testLock;
+
+void ThreadWrite()
 {
 	while (true)
 	{
+		testLock.TestPush();
+		this_thread::sleep_for(1ms);
+		testLock.TestPop();
+	}
+}
 
-		cout << "Hello ! I am thread... " << LThreadId << endl;
-		this_thread::sleep_for(1s);
-
+void ThreadRead()
+{
+	while (true)
+	{
+		int32 value = testLock.TestRead();
+		cout << value << endl;
+		this_thread::sleep_for(1ms);
 	}
 }
 
 int main()
 {
+	for (int32 i = 0; i < 2; i++)
+	{
+		GThreadManager->Launch(ThreadWrite);
+	}
+
 	for (int32 i = 0; i < 5; i++)
 	{
-		GThreadManager->Launch(ThreadMain);
+		GThreadManager->Launch(ThreadRead);
 	}
 
 	GThreadManager->Join();
