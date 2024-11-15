8287eca660fe7f8c5024329872f1464224d74005
tgparkk <sttgpark@gmail.com>
2024-11-15 16:47:08 +0900

    prime_number_count

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -7,30 +7,73 @@
 #include <future>
 #include "ThreadManager.h"
 
-#include "PlayerManager.h"
-#include "AccountManager.h"
+#include <vector>
+#include <thread>
+
+// 소수 구하기
+bool IsPrime(int number)
+{
+	if (number <= 1)
+		return false;
+	if (number == 2 || number == 3)
+		return true;
+
+	for (int i = 2; i < number; i++)
+	{
+		if ((number % i) == 0)
+			return false;
+	}
+
+	return true;
+}
+
+// [start ~ end]
+int CountPrime(int start, int end)
+{
+	int count = 0;
+
+	for (int number = start; number <= end; number++)
+	{
+		if (IsPrime(number))
+			count++;
+	}
+
+	return count;
+}
+
+// 1과 자기 자신으로만 나뉘면 그것을 소수라고 함
 
 int main()
 {
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "PlayerThenAccount" << endl;
-				GPlayerManager.PlayerThenAccount();
-				this_thread::sleep_for(100ms);
-			}
-		});
+	//const int MAX_NUMBER = 10'000;
+	//const int MAX_NUMBER = 1'000'000;
+	const int MAX_NUMBER = 10'000'000;
+	// 1~MAX_NUMBER까지의 소수 개수
+	vector<thread> threads;
 
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "AccountThenPlayer" << endl;
-				GAccountManager.AccountThenPlayer();
-				this_thread::sleep_for(100ms);
-			}
-		});
+	// 1000 = 168
+	// 10'000 = 1229
+	// 1'000'000 = 78498
+	// 10'000'000 = 664579
 
-	GThreadManager->Join();
+	int coreCount = thread::hardware_concurrency();
+	int jobCount = (MAX_NUMBER / coreCount) + 1;
+
+	atomic<int> primeCount = 0;
+
+	for (int i = 0; i < coreCount; i++)
+	{
+		int start = (i * jobCount) + 1;
+		int end = min(MAX_NUMBER, ((i + 1) * jobCount));
+
+		threads.push_back(thread([start, end, &primeCount]()
+			{
+				primeCount += CountPrime(start, end);
+			}));
+	}
+
+	for (thread& t : threads)
+		t.join();
+
+	cout << primeCount << endl;
 }
