e875421870d7a03e705167a3ae0b21215caf9501
tgparkk <sttgpark@gmail.com>
2024-11-16 18:08:06 +0900

    shared_ptr unique_ptr

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -9,11 +9,8 @@
 
 #include "RefCounting.h"
 
-using KnightRef = TSharedPtr<class Knight>;
-using InventoryRef = TSharedPtr<class Inventory>;
 
-class Knight : public RefCountable
-{
+class Knight{
 public:
 	Knight()
 	{
@@ -26,26 +23,11 @@ public:
 
 	}
 
-	void SetTarget(KnightRef target) {
-		_target = target;
-	}
 
-	KnightRef _target = nullptr;
-	InventoryRef _inventory = nullptr;
 };
 
 
-class Inventory : public RefCountable
-{
-public:
-	Inventory(KnightRef knight) : _knight(**knight)
-	{
 
-	}
-
-	// 참조로 인벤토리가 나이트를 가지고 있으면 문제 없음
-	Knight& _knight;
-};
 
 int main()
 {
@@ -53,10 +35,44 @@ int main()
 	// 1) 이미 만들어진 클래스 대상으로 사용 불가 - RefCountable 를 상송받아야 하기 때문에
 	// 2) 순환(Cycle) 문제 - std:: 스마트포인터도 문제
 
-	KnightRef k1(new Knight());
-	k1->ReleaseRef();
 
-	k1->_inventory = new Inventory(k1);
 
+	// unique_ptr
+	// 
+	// shared_ptr
+	// weak_ptr
+
+	// shared_ptr
+	/*
+	shaerd_ptr 로 생성시
+	[Knight][RefCountingBlock(uses, weak)]
+
+	[T*][RefCountingBlocking*]
+	위 두개의 구조가 생긴다.
+	*/
+
+	// [Knight][RefCountingBlock(usesCount(shared), weakCount)]
+	shared_ptr<Knight> spr(new Knight());
+
+	// 아래 코드는
+	/*
+	결과적으로
+	[Knight][RefCountingBlock]
+
+	[T*][RefCountingBlocking*]
+	[T*][RefCountingBlocking*]
+	
+	위 구조들이 생깁니다.
+	*/
+	shared_ptr<Knight> spr2 = spr;
+
+	// make_shared 는 [Knight | RefCountingBlock] 한 공간으로 생성
+	shared_ptr< Knight> sprm = make_shared<Knight>();
+
+
+	weak_ptr<Knight> wpr = spr;
+	
+	bool expired = wpr.expired();
+	shared_ptr<Knight> spr3 = wpr.lock();
 
 }
