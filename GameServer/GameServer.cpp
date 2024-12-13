bb7db696a9f6f8ba9ed7a59f4b51423470ea63dc
tgparkk <sttgpark@gmail.com>
2024-12-13 18:21:36 +0900

    code cleanup

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -12,18 +12,14 @@
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
-void HandleError(const char* cause)
-{
+void HandleError(const char* cause){
 	int32 errCode = ::WSAGetLastError();
 	cout << cause << " ErrorCode : " << errCode << endl;
 }
-
-int main()
-{
+int main(){
 	WSAData wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
-
 	// 블로킹(Blocking) 소켓
 	// accept -> 접속한 클라가 있을 때
 	// connect -> 서버 접속 성공했을 때
@@ -31,14 +27,11 @@ int main()
 	// recv, recvfrom -> 수신 버퍼에 도착한 데이터가 있고, 이를 유저레벨 버퍼에 복사했을 때
 
 	// 논블로킹(Non-Blocking)
-
 	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
-	if (listenSocket == INVALID_SOCKET)
-		return 0;
+	if (listenSocket == INVALID_SOCKET) return 0;
 
-	u_long on = 1;
-	if (::ioctlsocket(listenSocket, FIONBIO, &on) == INVALID_SOCKET)
-		return 0;
+	u_long on = 1; // 논 블록킹 소켓으로 설정
+	if (::ioctlsocket(listenSocket, FIONBIO, &on) == INVALID_SOCKET) return 0;
 
 	SOCKADDR_IN serverAddr;
 	::memset(&serverAddr, 0, sizeof(serverAddr));
@@ -46,74 +39,52 @@ int main()
 	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
 	serverAddr.sin_port = ::htons(7777);
 
-	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-		return 0;
-
-	if (::listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
-		return 0;
-
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) return 0;
+	
+	if (::listen(listenSocket, SOMAXCONN) == SOCKET_ERROR) return 0;
 	cout << "Accept" << endl;
 
 	SOCKADDR_IN clientAddr;
 	int32 addrLen = sizeof(clientAddr);
-
 	// Accept
-	while (true)
-	{
+	while (true){
 		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
-		if (clientSocket == INVALID_SOCKET)
-		{
-			// 원래 블록했어야 했는데... 너가 논블로킹으로 하라며?
-			if (::WSAGetLastError() == WSAEWOULDBLOCK)
-				continue;
+		if (clientSocket == INVALID_SOCKET){
+			// 원래 블록했어야 했는데... 너가 논블로킹으로 하라며?, 한번 더 체크
+			// 예를들어 recv 가 진행중
+			if (::WSAGetLastError() == WSAEWOULDBLOCK) continue;
 
 			// Error
 			break;
 		}
-
 		cout << "Client Connected!" << endl;
-
 		// Recv
-		while (true)
-		{
+		while (true){
 			char recvBuffer[1000];
 			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-			if (recvLen == SOCKET_ERROR)
-			{
+			if (recvLen == SOCKET_ERROR){
 				// 원래 블록했어야 했는데... 너가 논블로킹으로 하라며?
-				if (::WSAGetLastError() == WSAEWOULDBLOCK)
-					continue;
-
+				if (::WSAGetLastError() == WSAEWOULDBLOCK) continue;
 				// Error
 				break;
-			}
-			else if (recvLen == 0)
-			{
+			}else if (recvLen == 0){
 				// 연결 끊김
 				break;
 			}
-
 			cout << "Recv Data Len = " << recvLen << endl;
-
 			// Send
-			while (true)
-			{
-				if (::send(clientSocket, recvBuffer, recvLen, 0) == SOCKET_ERROR)
-				{
+			while (true){
+				if (::send(clientSocket, recvBuffer, recvLen, 0) == SOCKET_ERROR){
 					// 원래 블록했어야 했는데... 너가 논블로킹으로 하라며?
-					if (::WSAGetLastError() == WSAEWOULDBLOCK)
-						continue;
+					if (::WSAGetLastError() == WSAEWOULDBLOCK) continue;
 					// Error
 					break;
 				}
-
 				cout << "Send Data ! Len = " << recvLen << endl;
 				break;
 			}
 		}
 	}
-
-
 	// 윈속 종료
 	::WSACleanup();
 }
