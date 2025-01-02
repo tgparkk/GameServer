7d8f2ba8e7b45d973ef4eaa843fe12de4f25335d
tgparkk <sttgpark@gmail.com>
2025-01-02 23:39:08 +0900

    PacketSession

--- ServerCore/Session.h
+++ ServerCore/Session.h
@@ -92,3 +92,26 @@ private:
 	SendEvent			_sendEvent;
 };
 
+/*-----------------
+	PacketSession
+------------------*/
+
+struct PacketHeader
+{
+	uint16 size;
+	uint16 id; // 프로토콜ID (ex. 1=로그인, 2=이동요청)
+};
+
+class PacketSession : public Session
+{
+public:
+	PacketSession();
+	virtual ~PacketSession();
+
+	PacketSessionRef	GetPacketSessionRef() { 
+		return static_pointer_cast<PacketSession>(shared_from_this()); }
+
+protected:
+	virtual int32		OnRecv(BYTE* buffer, int32 len) sealed;
+	virtual int32		OnRecvPacket(BYTE* buffer, int32 len) abstract;
+};
